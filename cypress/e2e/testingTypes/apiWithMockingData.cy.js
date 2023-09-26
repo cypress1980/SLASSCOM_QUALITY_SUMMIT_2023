6b659b61fee30c96c3c8fc1bcc89c82aee060849
Kailash Chander Pathak <you@example.com>
2023-09-26 10:00:11 +0530

    test script updated

--- cypress/e2e/testingTypes/apiWithMockingData.cy.js
+++ cypress/e2e/testingTypes/apiWithMockingData.cy.js
@@ -1,23 +1,23 @@
-describe("Example to demonstrate API Mocking in Cypress using cy.intercept", () => {
+describe("API Mocking in Cypress using cy.intercept Methid ", () => {
   beforeEach(() => {
     cy.visit("https://angular.realworld.io/");
     cy.intercept("GET", "https://api.realworld.io/api/tags", {
-      fixture: "tags.json",
+      fixture: "mockTags.json",
     });
     cy.intercept(
       "GET",
       "https://api.realworld.io/api/articles?limit=10&offset=0",
-      { fixture: "articles.json" }
+      { fixture: "mockArticles.json" }
     );
   });
 
-  it("Mock the Tags from the API Response and then validate on UI", () => {
-    cy.get(".tag-list", { timeout: 10000 })
+  it("Mock API Tags, and then validate on UI", () => {
+    cy.get(".tag-list", { timeout: 1000 })
       .should("contain", "Cypress")
       .and("contain", "Playwright");
   });
 
-  it("Mock the Article feed from the API Response and then validate on UI", () => {
+  it("Mock the Article feed, and then validate on UI", () => {
     cy.get("app-favorite-button.pull-xs-right").contains("10");
     cy.get(".author").contains("Kailash Pathak");
     cy.get(".preview-link > p").contains("SLASSCOM QUALITY SUMMIT 2023");
